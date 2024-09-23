c82610e7801e21d354134ee1aedcb8e1ad17eaab
RaamakrishnanKV <raamakrishnankv@gmail.com>
2024-09-23 12:27:50 +0530

    All variables

--- week_001/firstfile.js
+++ week_001/firstfile.js
@@ -4,9 +4,177 @@
 // to validate all variables
 
 
+
+// ------------- STRING Starts ------------
 var name, gender, num1
 
 name = "Ramakrishnnan"
     console.log("The Name is "+name)
+    console.log('Type of name is' + typeof name);
+name = 'Vaidyanathan'
+    console.log("The Name is "+name)
+    console.log('Type of name is' + typeof name);
+name = `Raaja`
+    console.log("The Name is "+name)
+    console.log('Type of name is' + typeof name);
 
+ gender = "male"
+ console.log("The Gender is :"+gender) 
+ console.log('Type of gender is' + typeof name);
+
+ num1 = 200 // String to Number -- need to ask
+console.log('The Number is '+num1);
+console.log('Type of name is' + typeof num1);
+
+console.log("-------------------------------------------------------------")
+// ------------- STRING Ends------------    
+
+// ------------- NUMBER Starts ------------
+
+let num = 300
+
+console.log('The Number is '+num);
+console.log('Type of name is' + typeof num);
+console.log("-------------------------------------------------------------")
+
+// ------------- NUMBER Ends------------ 
+
+// Boolean Starts
+
+var isValid
+
+isValid = true 
+console.log('The value of Isvalid is '+isValid);
+console.log('The Type of isValid is '+typeof(isValid));
+
+isValid = false
+console.log('The value of Isvalid is '+isValid)+isValid;
+
+console.log('The value of Isvalid is ')+isValid;
+        // NOT REPORTINg ERROR -- WHY
+
+console.log('The Type of isValid is '+typeof(isValid));
+console.log("-------------------------------------------------------------")
+
+// Boolean ENDS
+
+// ---------------------------------------
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log('The value of Length is '+length+ 'The Value of Weight is'+weight);
+console.log('The Type of lenghth is '+typeof(length));
+console.log('The Type of Weight is '+typeof(Weight));
+
+console.log("-------------------------------------------------------------")
+
+
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color + lastName);
+
+console.log("-------------------------------------------------------------")
+
+// Booleans
+let x4 = true;
+let y1 = false;
+console.log((x4) + y1);
+console.log("-------------------------------------------------------------")
+
+
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person);
+
+console.log("The Value of person is :"+ typeof person);
+console.log("-------------------------------------------------------------")
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+console.log("The Value of cars is :"+ typeof cars);
+console.log("-------------------------------------------------------------")
+
+// Date object:
+const date = new Date("2022-03-25");
+console.log(date);
+console.log("The Value of cars is :"+ typeof date);
+
+console.log("-------------------------------------------------------------")
+
+let x1 = 16 + "Volvo";
+console.log(x1);
+console.log("The Value of cars is :"+ typeof x1);
+
+console.log("-------------------------------------------------------------")
+
+
+
+let x2 = "Volvo" + 16;
+console.log(x2);
+console.log("The Value of cars is :"+ typeof x2);
+
+console.log("-------------------------------------------------------------")
+
+
+
+let x3 = 16 + 4 + "Volvo";
+console.log(x3);
+console.log("The Value of cars is :"+ typeof x3);
+console.log("-------------------------------------------------------------")
+
+
+let name1;       // Now x is undefined
+console.log(name1);
+console.log("The Value of cars is :"+ typeof name1);
+console.log("-------------------------------------------------------------")
+
+
+name1 = 5;       // Now x is a Number
+name1 = "John";  // Now x is a String
+console.log(name1);
+console.log("The Value of cars is :"+ typeof name1);
+console.log("-------------------------------------------------------------")
+
+
+console.log(name1 = "John");  // Now x is a String
+
+console.log("The Value of cars is :"+ typeof name1);
+console.log("-------------------------------------------------------------")
+
+
+
+let answer2 = "He is called 'Johnny'";
+
+console.log(answer2);  // Now x is a String
+console.log("The Value of answer2 is :"+ typeof answer2);
+console.log("-------------------------------------------------------------")
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+
+console.log(answer3);  // Now x is a String
+
+console.log("The Value of cars is :"+ typeof answer3);
+console.log("-------------------------------------------------------------")
+
+
+let x6 = "5" + 2 + 3;
+
+console.log(x6);  // Now x is a String
+
+console.log("The Value of cars is :"+ typeof x6);
+console.log("-------------------------------------------------------------")
+
+
+let x5 = "John" + " " + "Doe";
+
+console.log(x5);  // Now x is a String
+
+console.log("The Value of cars is :"+ typeof x5);
+console.log("-------------------------------------------------------------")
 
